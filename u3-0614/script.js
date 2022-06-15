32e91bffa62d628cb221816a5e38067d2d3bd34e
admin <gerimantas.v@gmail.com>
2022-06-15 14:59:01 +0300

    3-ios uzduoties klaida

--- u3-0614/script.js
+++ u3-0614/script.js
@@ -1,3 +1,16 @@
 window.onload = function() {
-    
+    let tMasyvas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    console.log(tMasyvas);
+
+    console.log("Kas antras masyvo skaicius: "+kasAntras(masyvas));
+}
+
+
+
+function kasAntras(masyvas) {
+    const masyvas = [];
+    for (let i = 0; i <= masyvas.length; i += 2) {
+        masyvas.push(i);
+    }
+    return masyvas;
 }
