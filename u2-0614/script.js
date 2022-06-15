db68bce03ead48d466fa8a55a0b3b24ac733c34d
admin <gerimantas.v@gmail.com>
2022-06-15 14:39:22 +0300

    Baigta 2-a uzduotis

--- u2-0614/script.js
+++ u2-0614/script.js
@@ -1,4 +1,14 @@
 window.onload = function() {
-    let testas = [3,5,2,1];
+    let testas = [3,5,2,1,2,2,3];
     console.log(testas);
+
+    console.log("Masyvo sandauga = " +masyvoSandauga(testas))
+}
+
+function masyvoSandauga(masyvas) {
+    let sandauga = 1;
+    for (let i = 0; i < masyvas.length; i++) {
+        sandauga *= masyvas[i];
+    }
+    return sandauga;
 }
