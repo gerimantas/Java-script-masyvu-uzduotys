6ff5d14c2fa78c7712988cc90f44eb4e3bcaaa5d
admin <gerimantas.v@gmail.com>
2022-06-16 14:55:22 +0300

    Pradeta 4-a uzduotis

---
+++ u4-0614/script.js
@@ -0,0 +1,14 @@
+window.onload =function() {
+ 
+   
+   console.log(isvestiNelyginiusNuo107iki309());
+}
+
+function isvestiNelyginiusNuo107iki309() {
+    const nelyginiai = [];
+    for (let i = 107; i <= 309; i+=2) {  
+        nelyginiai.push(i); 
+    }
+    
+    return nelyginiai;
+}
