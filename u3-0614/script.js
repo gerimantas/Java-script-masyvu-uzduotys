49f1cae90fc6d11c3f4bc4ebf19c6ded39328e57
admin <gerimantas.v@gmail.com>
2022-06-16 12:26:16 +0300

    Baigta 3-ia masyvu u\zduotis

--- u3-0614/script.js
+++ u3-0614/script.js
@@ -2,15 +2,15 @@ window.onload = function() {
     let tMasyvas = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     console.log(tMasyvas);
 
-    console.log("Kas antras masyvo skaicius: "+kasAntras(masyvas));
+    console.log("Kas antras masyvo skaicius: "+kasAntras(tMasyvas));
 }
 
 
 
-function kasAntras(masyvas) {
-    const masyvas = [];
-    for (let i = 0; i <= masyvas.length; i += 2) {
-        masyvas.push(i);
+function kasAntras(tMasyvas) {
+    const masyvasA = [];
+    for (let i = 2; i <= tMasyvas.length; i += 2) {
+        masyvasA.push(i);
     }
-    return masyvas;
+    return masyvasA;
 }
